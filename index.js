37a091006e243ebc1cb31e273beb5682e24fa740
Javi del Castillo <15940847+fjavidcr@users.noreply.github.com>
2024-03-15 17:44:36 +0000

    feat: tests gemini pro vision

---
+++ index.js
@@ -0,0 +1,6 @@
+const createNonStreamingMultipartContent = require('./src/vision.js')
+const generateContent = require('./src/gemini-pro.js')
+
+// createNonStreamingMultipartContent()
+
+generateContent()
